fec910db749f20defa0725e4250a81b11572b4ad
YSChandu <yarrapothusaichandu@gmail.com>
2025-04-04 20:33:03 +0530

    added info

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,8 +27,8 @@ export function Hero() {
           <a href="#projects" className="bg-primary text-primary-foreground px-6 py-3 rounded-lg hover:bg-primary/90 transition-colors">
             View Projects
           </a>
-          <a href="https://drive.google.com/file/d/1kFFkdbhLQ2KyXh1qVDzsB_JleKFYH2H-/view?usp=sharing" className="bg-secondary text-secondary-foreground px-6 py-3 rounded-lg hover:bg-secondary/80 transition-colors">
-            Download Resume
+          <a href="https://drive.google.com/file/d/17DdUvP2YV37FxKusTuBrx24-sv2Li6iE/view?usp=sharing" className="bg-secondary text-secondary-foreground px-6 py-3 rounded-lg hover:bg-secondary/80 transition-colors">
+            View Resume
           </a>
         </div>
 
